f25ca8e9e0dd011daa6b4473c3bf3567952f0392
Sanawar_21 <sanawarsaeed@gmail.com>
2024-09-21 15:42:49 +0500

    remove sheets client (found an alternative way to download excel files from google drive)

--- package/__init__.py
+++ package/__init__.py
@@ -1,5 +1,4 @@
 from .client import BaseClient
 from .salesforce import AbstractSalesforceClient, GradesClient
-from .sheets import SheetsClient
 from .grade import GradeEntry, GradeReader
 from .drive import DriveClient
